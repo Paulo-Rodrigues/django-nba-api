a2600e9ca3bb28242d8bbac7a575008c26515b72
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-06-22 21:18:59 -0300

    add players class

--- players/models.py
+++ players/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from nba_api.stats.static import players
 
-# Create your models here.
+class Player:
+    def find_player_by_full_name(self, name):
+        return players.find_players_by_full_name(name)
+
+    def find_player_by_first_name(self, name):
+        return players.find_players_by_first_name(name)
+
+    def find_player_by_last_name(self,name):
+        return players.find_players_by_last_name(name)
+
+    def all(self):
+        return players.get_players()
+
